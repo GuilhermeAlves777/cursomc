e0faf5105a7ccaf79af0c41f202124ee6e7fd4e4
Guilherme Alves <aguilherme787@gmail.com>
2024-12-29 18:57:58 -0300

    Versão Inicial

---
+++ src/main/java/com/guilhermealves/cursomc/CursomcApplication.java
@@ -0,0 +1,13 @@
+package com.guilhermealves.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
