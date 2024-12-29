f0df78702e0f09b5d255922952653d974a1ff2b6
Guilherme Alves <aguilherme787@gmail.com>
2024-12-29 19:28:33 -0300

    Testando o REST

---
+++ src/main/java/com/guilhermealves/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,18 @@
+package com.guilhermealves.cursomc.resources;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	//Associação com um verbo do HTTP para busca de dados
+	@GetMapping
+	
+	// Verificação se o REST está funcionando
+	public String listar() {
+		return "REST está funcionando";
+	}
+	
+}
