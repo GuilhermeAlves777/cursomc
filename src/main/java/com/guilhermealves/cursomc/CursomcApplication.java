5e8f00e595c85e02d413a2f1b608afb3548c0fd5
Guilherme Alves <aguilherme787@gmail.com>
2025-01-03 22:34:12 -0300

    Adição dos estados e cidades

--- src/main/java/com/guilhermealves/cursomc/CursomcApplication.java
+++ src/main/java/com/guilhermealves/cursomc/CursomcApplication.java
@@ -10,8 +10,12 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.guilhermealves.cursomc.domain.Categoria;
+import com.guilhermealves.cursomc.domain.Cidade;
+import com.guilhermealves.cursomc.domain.Estado;
 import com.guilhermealves.cursomc.domain.Produto;
 import com.guilhermealves.repositories.CategoriaRepository;
+import com.guilhermealves.repositories.CidadeRepository;
+import com.guilhermealves.repositories.EstadoRepository;
 import com.guilhermealves.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -25,6 +29,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -53,6 +61,19 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		Estado est1 = new Estado (null, "Minas Gerais");
+		Estado est2 = new Estado (null, "São Paulo");
+		
+		Cidade c1 = new Cidade (null, "Ubêrlandia", est1);
+		Cidade c2 = new Cidade (null, "São Paulo", est2);
+		Cidade c3 = new Cidade (null,"Campinas",est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		
 		
 	}
---
+++ src/main/java/com/guilhermealves/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.guilhermealves.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.guilhermealves.cursomc.domain.Estado;
+
+//Esta classe poderá fazer ações de banco de dados (Criar, alterar, excluir) em todos os objetos do tipo categoria
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
